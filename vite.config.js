0d1cd96e41de616f076ef9cb38f73e6b77d46642
xl-xlxl <xlxl0495@outlook.com>
2025-04-09 01:58:12 +0800

    看看vite.config.js缺不缺东西

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,29 @@
+
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignXVueResolver } from 'ant-design-x-vue/resolver';
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api/qianfan': {
-        target: 'https://qianfan.baidubce.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/qianfan/, '')
-      }
+    plugins: [vue(), 
+                tailwindcss(), 
+                Components({resolvers: [AntDesignXVueResolver()]})
+            ],
+    server: {
+        proxy: {
+            '/api/qianfan': {
+                target: 'https://qianfan.baidubce.com',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api\/qianfan/, '')
+            }
+        }
+    },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
 });
