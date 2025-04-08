87124829dc5cad5a8788986b19de7d150e7bd41c
Almighty <kz@gdmu.edu.cn>
2025-04-08 23:25:05 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,32 @@
-import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
-// https://vitejs.dev/config/
+import Components from 'unplugin-vue-components/vite'
+
+import { AntDesignXVueResolver } from 'ant-design-x-vue/resolver';
+
 export default defineConfig({
-  plugins: [
-    vue(),
-    // vueDevTools(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    plugins: [
+        vue(),
+        Components({
+
+            dirs: ['src/components'],
+
+            extensions: ['vue'],
+
+            dts: 'src/components.d.ts',
+
+            resolvers: [
+
+                AntDesignXVueResolver(),
+
+            ],
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src')
+        }
+    }
 })
