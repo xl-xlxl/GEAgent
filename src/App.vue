04f1f3279bb9801d5134950dba1f8783d572b026
xl-xlxl <xlxl0495@outlook.com>
2025-04-12 21:03:48 +0800

    暂存

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,105 @@
 <template>
   <a-layout style="min-height: 100vh; user-select: none;">
     <a-layout-sider v-model:collapsed="collapsed" collapsible :width="260" :collapsedWidth="70">
-      <div class="icon-container" @click="toggleCollapsed" :class="{ collapsed: collapsed }">
-        <!-- 展开时显示文字 -->
-        <span v-if="!collapsed" class="title">GESeek</span>
-        <img src="/收起.svg" alt="logo" class="icon -mb-1" />
-      </div>
-      <!-- 新增按钮 -->
-      <div class="icon-container" :class="{ collapsed: collapsed }">
-        <img src="/新增对话.svg" alt="add" class="icon" />
+
+      <div>
+        <div class="icon-container" @click="toggleCollapsed" :class="{ collapsed: collapsed }">
+          <!-- 展开时显示文字 -->
+          <span v-if="!collapsed" class="title">GESeek</span>
+          <img src="/收起.svg" alt="close" class="icon -mb-1" />
+        </div>
       </div>
 
-      <a-popover trigger="click">
-        <template #content>
-          <div class="no-select">
-            <h1 style=" font-weight: bold; margin-bottom: 15px;">模型设置</h1>
-            <label>
-              <a-tooltip title="数值越高，模型可输入与输出文本长度越长；数值越低，模型可输入与输出文本长度越短">
-                <span style="cursor: pointer; color: #1890ff;">!</span>
-              </a-tooltip>
-              最大生成长度:
-              <a-slider v-model:value="max_tokens" :step="1" :min="1000" :max="10000" @change="switchSettings" style="width: 250px;" />
-            </label>
-            <label>
-              <a-tooltip title="数值越高，模型输出越随机，创造力越强；数值越低，输出越确定">
-                <span style="cursor: pointer; color: #1890ff;">!</span>
-              </a-tooltip>
-              创造力:
-              <a-slider v-model:value="temperature" :step="0.1" :min="0" :max="1" @change="switchSettings" />
-            </label>
-            <label>
-              <a-tooltip title="数值越高，生成的文本多样性越强；数值越低，生成的文本越集中在高概率的词汇上">
-                <span style="cursor: pointer; color: #1890ff;">!</span>
-              </a-tooltip>
-              多样性:
-              <a-slider v-model:value="top_p" :step="0.1" :min="0" :max="1" @change="switchSettings" />
-            </label>
-            <label>
-              <a-tooltip title="数值越大，模型从更多候选词中选择词汇，生成的文本可能更丰富；数值越小，模型从较少候选词中选择词汇，生成的文本可能更稳定">
-                <span style="cursor: pointer; color: #1890ff;">!</span>
-              </a-tooltip>
-              候选词范围:
-              <a-slider v-model:value="top_k" :step="1" :min="10" :max="100" @change="switchSettings" />
-            </label>
+      <div>
+        <!-- 新增按钮 -->
+        <div v-if="!collapsed">
+          <div class="bubble icon-container" :class="{ collapsed: collapsed }">
+            开启新的对话
+            <img src="/新增对话.svg" alt="add" class="icon" />
           </div>
-        </template>
-        <div class="icon-container fixed-bottom settings" :class="{ collapsed: collapsed }">
-          <img src="/设置.svg" alt="settings" class="icon" />
         </div>
-      </a-popover>
+        <div v-else>
+          <div class="icon-container" :class="{ collapsed: collapsed }">
+            <img src="/新增对话.svg" alt="add" class="icon" />
+          </div>
+        </div>
+      </div>
 
-      <a-popover trigger="click">
-        <template #content>
-          <p>Content</p>
-          <p>Content</p>
-        </template>
-        <div class="icon-container fixed-bottom user" :class="{ collapsed: collapsed }">
-          <img src="/用户.svg" alt="user" class="icon" />
-        </div>
-      </a-popover>
+      <div>
+
+      </div>
+
+      <div>
+        <a-popover trigger="click" class="fixed-bottom settings">
+          <template #content>
+            <div class="no-select">
+              <h1 style=" font-weight: bold; margin-bottom: 15px;">模型设置</h1>
+              <label>
+                <a-tooltip title="数值越高，模型可输入与输出文本长度越长；数值越低，模型可输入与输出文本长度越短">
+                  <span style="cursor: pointer; color: #1890ff;">!</span>
+                </a-tooltip>
+                最大生成长度:
+                <a-slider v-model:value="max_tokens" :step="1" :min="1000" :max="10000" @change="switchSettings"
+                  style="width: 250px;" />
+              </label>
+              <label>
+                <a-tooltip title="数值越高，模型输出越随机，创造力越强；数值越低，输出越确定">
+                  <span style="cursor: pointer; color: #1890ff;">!</span>
+                </a-tooltip>
+                创造力:
+                <a-slider v-model:value="temperature" :step="0.1" :min="0" :max="1" @change="switchSettings" />
+              </label>
+              <label>
+                <a-tooltip title="数值越高，生成的文本多样性越强；数值越低，生成的文本越集中在高概率的词汇上">
+                  <span style="cursor: pointer; color: #1890ff;">!</span>
+                </a-tooltip>
+                多样性:
+                <a-slider v-model:value="top_p" :step="0.1" :min="0" :max="1" @change="switchSettings" />
+              </label>
+              <label>
+                <a-tooltip title="数值越大，模型从更多候选词中选择词汇，生成的文本可能更丰富；数值越小，模型从较少候选词中选择词汇，生成的文本可能更稳定">
+                  <span style="cursor: pointer; color: #1890ff;">!</span>
+                </a-tooltip>
+                候选词范围:
+                <a-slider v-model:value="top_k" :step="1" :min="10" :max="100" @change="switchSettings" />
+              </label>
+            </div>
+          </template>
+          <div v-if="!collapsed">
+            <div class="bubble icon-container" :class="{ collapsed: collapsed }">
+              模型设置
+              <img src="/设置.svg" alt="user" class="icon" />
+            </div>
+          </div>
+          <div v-else>
+            <div class="icon-container" :class="{ collapsed: collapsed }">
+              <img src="/设置.svg" alt="user" class="icon" />
+            </div>
+          </div>
+        </a-popover>
+      </div>
+
+      <div>
+        <a-popover trigger="click" class="fixed-bottom user">
+          <template #content>
+            <div class="no-select">
+              <h1 style=" font-weight: bold; margin-bottom: 15px;">修改信息</h1>
+              <h1 style=" font-weight: bold; margin-bottom: 15px;">退出登录</h1>
+            </div>
+          </template>
+          <div v-if="!collapsed">
+            <div class="bubble icon-container" :class="{ collapsed: collapsed }">
+              <a-avatar :size="40" src=""></a-avatar>
+              <img src="/用户.svg" alt="user" class="icon" />
+            </div>
+          </div>
+          <div v-else>
+            <div class="icon-container" :class="{ collapsed: collapsed }">
+              <img src="/用户.svg" alt="user" class="icon" />
+            </div>
+          </div>
+        </a-popover>
+      </div>
 
     </a-layout-sider>
     <div class="app-container">
@@ -167,13 +208,13 @@ export default {
 
 /* 设置按钮样式 */
 .fixed-bottom.settings {
-  bottom: 56px;
+  bottom: 60px;
   /* 设置按钮距离底部的间距 */
 }
 
 /* 用户按钮样式 */
 .fixed-bottom.user {
-  bottom: 16px;
+  bottom: 1px;
   /* 用户按钮距离底部的间距 */
 }
 
@@ -204,5 +245,14 @@ export default {
 ::v-deep(.ant-slider-track) {
   background-color: #E8F8FF !important;
   /* 滑动条选中部分的颜色 */
+}
+
+.bubble {
+  max-width: 99%;
+  background: linear-gradient(135deg, #FFECE8, #E8F8FF, #F4FFE8);
+  padding: 8px 16px;
+  border-radius: 20px;
+  color: #777777;
+  font-weight: bold
 }
 </style>
