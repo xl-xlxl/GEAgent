ecbb3f0586d96e53da80f0d1d68d1801942d0bce
xl-xlxl <xlxl0495@outlook.com>
2025-04-23 11:50:04 +0800

    修复报错

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,16 @@ import path from 'path'
 import Components from 'unplugin-vue-components/vite'
 import { AntDesignXVueResolver } from 'ant-design-x-vue/resolver';
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+
 
 export default defineConfig({
     plugins: [
         vue(),
         tailwindcss(),
+        nodePolyfills({
+            include: ['path', 'fs', 'url', 'source-map-js']
+        }),
         Components({
             dirs: ['src/components'],
             extensions: ['vue'],
@@ -39,6 +44,7 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
+            'source-map-js': 'source-map',
         }
     },
 })
